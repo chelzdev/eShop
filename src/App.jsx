ffa7a2d4a7ea8e3dfb80259de0c05d1faefd2cd6
Austine Numbi Chelsea <chelz@Austines-MacBook-Pro.local>
2024-07-15 17:04:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+
+
+import Header from "./Header";
+
+function App() {
+  return (
+    
+          <div className="App">
+            <Header />
+      
+      </div>
+  
+  );
+}
+
+export default App;
